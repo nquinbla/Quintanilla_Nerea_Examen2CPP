34acf6f3772f20ad957652ea98448a644f0144eb
nquinbla <nquinbla@myuax.com>
2023-12-14 12:08:54 +0100

    a

--- Estructura del estudiante/mainc++.cpp
+++ Estructura del estudiante/mainc++.cpp
@@ -1,21 +1,26 @@
-// Estructura del estudiante
+// 1. Estructura del estudiante con C++
 // a. Implementa una estructura llamada Estudiante con los campos: nombre, edad, y promedio.
 // b. Desarrolla la función mostrarEstudiante() que muestre todos los campos de la estructura.
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 // a. Estructura llamada Estudiante con los campos: nombre, edad, y promedio.
+
 struct Estudiante
 {
-    char nombre[30];
+    string nombre;
     int edad;
     float promedio;
 };
 
 // b. Función mostrarEstudiante() que muestre todos los campos de la estructura.
+
 void mostrarEstudiante(Estudiante estudiante)
 {
-    printf("Nombre: %s\n", estudiante.nombre);
-    printf("Edad: %d\n", estudiante.edad);
-    printf("Promedio: %f\n", estudiante.promedio);
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
 }
--- Estructura del estudiante/mainc.cpp
+++ Estructura del estudiante/mainc.cpp
@@ -1,26 +1,21 @@
-// Estructura del estudiante
+// 2. Estructura del estudiante con C
 // a. Implementa una estructura llamada Estudiante con los campos: nombre, edad, y promedio.
 // b. Desarrolla la función mostrarEstudiante() que muestre todos los campos de la estructura.
 
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 // a. Estructura llamada Estudiante con los campos: nombre, edad, y promedio.
-
 struct Estudiante
 {
-    string nombre;
+    char nombre[30];
     int edad;
     float promedio;
 };
 
 // b. Función mostrarEstudiante() que muestre todos los campos de la estructura.
-
 void mostrarEstudiante(Estudiante estudiante)
 {
-    cout << "Nombre: " << estudiante.nombre << endl;
-    cout << "Edad: " << estudiante.edad << endl;
-    cout << "Promedio: " << estudiante.promedio << endl;
+    printf("Nombre: %s\n", estudiante.nombre);
+    printf("Edad: %d\n", estudiante.edad);
+    printf("Promedio: %f\n", estudiante.promedio);
 }
