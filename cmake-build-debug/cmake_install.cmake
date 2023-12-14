152ae3e174f2f7be0c08eeb4f093951fbd246c2c
nquinbla <nquinbla@myuax.com>
2023-12-14 12:14:13 +0100

    arreglando cosillas

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/nerea/OneDrive/Documentos/GitHub/Quintanilla_Nerea_Examen2CPP/Estructura del estudiante
+# Install script for directory: C:/Users/nerea/OneDrive/Documentos/GitHub/Quintanilla_Nerea_Examen2CPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/nerea/OneDrive/Documentos/GitHub/Quintanilla_Nerea_Examen2CPP/Estructura del estudiante/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/nerea/OneDrive/Documentos/GitHub/Quintanilla_Nerea_Examen2CPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
